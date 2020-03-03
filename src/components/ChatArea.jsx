3c022a686a18abe91141fd578f984c09bf784cd9
enmalafeev <malafeev.en@yandex.ru>
2020-03-03 23:04:22 +0300

    refactor ChatArea

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -1,33 +1,28 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import InputMessage from './InputMessage';
-import { connect } from 'react-redux';
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const props = {
     messages: state.messages,
-  }
-  return props;
-}
-
-class ChatArea extends React.Component {
-  render() {
-    const { messages } = this.props;
-    const renderMessages = () => (
-      <ul className="h-100 list-group">
-        {
-          messages.map(({ id, userMessage }) => (
-          <li className="list-group-item" key={id}>{userMessage}</li>))
-        }
-      </ul>
-    );
-    console.log(messages);
-    return (
-      <div className="ChatArea col-9">
-        {renderMessages()}
-        <InputMessage />
-      </div>
-    )
   };
+  return props;
 };
+
+const renderMessages = (messages) => (
+  <ul className="h-100 list-group">
+    {
+      messages.map(({ id, userMessage }) => (
+        <li className="list-group-item" key={id}>{userMessage}</li>))
+    }
+  </ul>
+);
+
+const ChatArea = ({ messages }) => (
+  <div className="ChatArea col-9">
+    {renderMessages(messages)}
+    <InputMessage />
+  </div>
+);
 
 export default connect(mapStateToProps)(ChatArea);
