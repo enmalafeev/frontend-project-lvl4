2c96023a914742e9e291b86894a8586999c97548
enmalafeev <malafeev.en@yandex.ru>
2020-04-26 15:46:12 +0300

    add btns for channels

--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import cn from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { connect } from 'react-redux';
 import { actions } from '../slices';
 
@@ -50,18 +52,40 @@ class ChannelsList extends React.Component {
         </div>
         <ul className="nav flex-column nav-pills nav-fill">
           {
-            channels.map(({ id, name }) => (
-              <li key={id}>
-                <button
-                  type="button"
-                  className={activeClass(id)}
-                  onClick={this.handleClick(id)}
-                  onKeyDown={this.handleClick(id)}
-                >
-                  {name}
-                </button>
-              </li>
-            ))
+            channels.map(({ id, name, removable }) => {
+              if (removable) {
+                return (
+                  <li key={id} className="d-inline-flex">
+                    <button
+                      type="button"
+                      className={activeClass(id)}
+                      onClick={this.handleClick(id)}
+                      onKeyDown={this.handleClick(id)}
+                    >
+                      {name}
+                    </button>
+                    <button type="button">
+                      <FontAwesomeIcon icon={faEdit} />
+                    </button>
+                    <button type="button">
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </li>
+                );
+              }
+              return (
+                <li key={id} className="d-inline-flex">
+                  <button
+                    type="button"
+                    className={activeClass(id)}
+                    onClick={this.handleClick(id)}
+                    onKeyDown={this.handleClick(id)}
+                  >
+                    {name}
+                  </button>
+                </li>
+              );
+            })
           }
         </ul>
       </div>
