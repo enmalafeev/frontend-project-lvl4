939d9d09f22dd563326d6f23a4a1011f30b132d8
enmalafeev <malafeev.en@yandex.ru>
2020-05-08 15:47:52 +0300

    refactor on hooks ChannelsList.jsx

--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -2,95 +2,83 @@ import React from 'react';
 import cn from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { actions } from '../slices';
 
-const mapStateToProps = (state) => {
-  const props = {
-    channels: state.channels.channels,
-    currentChannel: state.currentChannel.id,
+const ChannelsList = () => {
+  const channels = useSelector((state) => state.channels.channels);
+  const currentChannel = useSelector((state) => state.currentChannel.id);
+
+  const dispatch = useDispatch();
+
+  const handleClick = (id) => (e) => {
+    e.preventDefault();
+    dispatch(actions.setActiveChannel(id));
   };
-  return props;
-};
 
-const actionCreators = {
-  setActiveChannel: actions.setActiveChannel,
-  showModal: actions.showModal,
-};
-
-class ChannelsList extends React.Component {
-  handleClick = (id) => (e) => {
+  const handleShowModal = (type, channel = null) => (e) => {
     e.preventDefault();
-    const { setActiveChannel } = this.props;
-    setActiveChannel(id);
-  }
+    dispatch(actions.showModal({ type, channel }));
+  };
 
-  handleShowModal = (type, channel = null) => (e) => {
-    e.preventDefault();
-    const { showModal } = this.props;
-    showModal({ type, channel });
-  }
+  const activeClass = (id) => cn({
+    'list-group-item': true,
+    'w-100': true,
+    active: currentChannel === id,
+  });
 
-  render() {
-    const { channels, currentChannel } = this.props;
-    const activeClass = (id) => cn({
-      'list-group-item': true,
-      'w-100': true,
-      active: currentChannel === id,
-    });
-    return (
-      <div className="col-3 border-right">
-        <div className="d-flex mb-2">
-          <span>Channels</span>
-          <button
-            type="button"
-            className="btn btn-link p-0 ml-auto"
-            onClick={this.handleShowModal('addChannel')}
-          >
-            +
-          </button>
-        </div>
-        <ul className="nav flex-column nav-pills nav-fill">
-          {
-            channels.map((channel) => {
-              if (channel.removable) {
-                return (
-                  <li key={channel.id} className="d-inline-flex">
-                    <button
-                      type="button"
-                      className={activeClass(channel.id)}
-                      onClick={this.handleClick(channel.id)}
-                      onKeyDown={this.handleClick(channel.id)}
-                    >
-                      {channel.name}
-                    </button>
-                    <button type="button" onClick={this.handleShowModal('renameChannel', channel)}>
-                      <FontAwesomeIcon icon={faEdit} />
-                    </button>
-                    <button type="button" onClick={this.handleShowModal('removeChannel', channel)}>
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                  </li>
-                );
-              }
+  return (
+    <div className="col-3 border-right">
+      <div className="d-flex mb-2">
+        <span>Channels</span>
+        <button
+          type="button"
+          className="btn btn-link p-0 ml-auto"
+          onClick={handleShowModal('addChannel')}
+        >
+          +
+        </button>
+      </div>
+      <ul className="nav flex-column nav-pills nav-fill">
+        {
+          channels.map((channel) => {
+            if (channel.removable) {
               return (
                 <li key={channel.id} className="d-inline-flex">
                   <button
                     type="button"
                     className={activeClass(channel.id)}
-                    onClick={this.handleClick(channel.id)}
-                    onKeyDown={this.handleClick(channel.id)}
+                    onClick={handleClick(channel.id)}
+                    onKeyDown={handleClick(channel.id)}
                   >
                     {channel.name}
                   </button>
+                  <button type="button" onClick={handleShowModal('renameChannel', channel)}>
+                    <FontAwesomeIcon icon={faEdit} />
+                  </button>
+                  <button type="button" onClick={handleShowModal('removeChannel', channel)}>
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
                 </li>
               );
-            })
-          }
-        </ul>
-      </div>
-    );
-  }
-}
+            }
+            return (
+              <li key={channel.id} className="d-inline-flex">
+                <button
+                  type="button"
+                  className={activeClass(channel.id)}
+                  onClick={handleClick(channel.id)}
+                  onKeyDown={handleClick(channel.id)}
+                >
+                  {channel.name}
+                </button>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, actionCreators)(ChannelsList);
+export default ChannelsList;
